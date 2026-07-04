e85b734a27fe8d95706ec3d682c90a0916a0027e
Kyrylo Prylipko <24914627+reend@users.noreply.github.com>
2026-07-04 16:13:22 +0200

    size macros

--- Src/main.c
+++ Src/main.c
@@ -22,8 +22,60 @@
   #warning "FPU is not initialized, but the project is compiling for an FPU. Please initialize the FPU before use."
 #endif
 
+#include <stdio.h>
+
+void task1_handler(void);
+void task2_handler(void);
+void task3_handler(void);
+void task4_handler(void);
+
+#define SIZE_TASK_STACK		1024U
+#define SIZE_SCHED_STACK 	1024U
+
+#define SRAM_START			0x20000000U
+#define SIZE_SRAM			((128) * (1024))
+#define SRAM_END			((SRAM_START) + (SIZE_SRAM))
+
+#define T1_STACK_START		SRAM_END
+#define T2_STACK_START		((SRAM_END) - (1 * SIZE_TASK_STACK))
+#define T3_STACK_START		((SRAM_END) - (2 * SIZE_TASK_STACK))
+#define T4_STACK_START		((SRAM_END) - (3 * SIZE_TASK_STACK))
+#define SCHED_STACK_START	((SRAM_END) - (4 * SIZE_TASK_STACK))
+
 int main(void)
 {
     /* Loop forever */
 	for(;;);
+}
+
+void task1_handler(void)
+{
+	while(1)
+	{
+		printf("Task 1\n");
+	}
+}
+
+void task2_handler(void)
+{
+	while(1)
+	{
+		printf("Task 2\n");
+	}
+}
+
+void task3_handler(void)
+{
+	while(1)
+	{
+		printf("Task 3\n");
+	}
+}
+
+void task4_handler(void)
+{
+	while(1)
+	{
+		printf("Task 4\n");
+	}
 }
